331f79a68bb30c4f4696ad45c45ce01ce6179e83
Lauren Days <lmchambers0420@gmail.com>
2019-06-21 03:05:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,15 @@ class Scraper
     self.get_page.css(".post")
   end
   
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.title = post.css(".date").text
+      course.schedule = post.css("p").text 
+    end 
+  end 
+  
     def print_courses
     self.make_courses
     Course.all.each do |course|
